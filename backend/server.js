45e98937b14f700c9ccd2f28711060ee3977bad1
minormist <achyutmsharma@gmail.com>
2024-10-21 01:47:38 +0530

    Basic structure

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+app.listen(5000, () => {
+  console.log("Server started at https://localhost:5000");
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is up");
+});
---
+++ readme.md
@@ -0,0 +1,11 @@
+Backend -
+terminal ./backend --
+
+npm install
+
+Frontend -
+terminal ./frontend --
+
+cd @latest
+npm install
+npm run dev
